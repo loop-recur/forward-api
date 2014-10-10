35866380ada2f7de7e6830755b193f6fc28cfdfa
Joe Nelson <cred+github@begriffs.com>
2014-10-10 15:39:55 -0700

    A few more anon privs

--- deploy/anonymous_user.sql
+++ deploy/anonymous_user.sql
@@ -5,9 +5,9 @@
 
 BEGIN;
 
-CREATE ROLE forward_anonymous;
+CREATE ROLE forward_anonymous INHERIT;
 
 GRANT USAGE ON SCHEMA "1" TO forward_anonymous;
-GRANT SELECT, REFERENCES ON ALL TABLES IN SCHEMA "1" TO forward_anonymous;
+GRANT SELECT, REFERENCES ON SCHEMA information_schema to forward_anonymous;
 
 COMMIT;
--- revert/anonymous_user.sql
+++ revert/anonymous_user.sql
@@ -4,6 +4,7 @@ BEGIN;
 
 REVOKE ALL ON SCHEMA "1" FROM forward_anonymous;
 REVOKE ALL ON ALL TABLES IN SCHEMA "1" FROM forward_anonymous;
+
 DROP ROLE forward_anonymous;
 
 COMMIT;
